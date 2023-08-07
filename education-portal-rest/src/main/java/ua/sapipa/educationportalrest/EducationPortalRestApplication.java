b35a3b396ab38a83c1d1c3fe82d488042fdcf4e7
Yevhenii Kravchenko <yevhenii.kravchenko@aimprosoft.com>
2023-08-07 19:19:51 +0300

    feat: init gradle project, modify gitignore

---
+++ education-portal-rest/src/main/java/ua/sapipa/educationportalrest/EducationPortalRestApplication.java
@@ -0,0 +1,13 @@
+package ua.sapipa.educationportalrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPortalRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationPortalRestApplication.class, args);
+    }
+
+}
---
+++ education-portal-rest/src/test/java/ua/sapipa/educationportalrest/EducationPortalRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.sapipa.educationportalrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPortalRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
